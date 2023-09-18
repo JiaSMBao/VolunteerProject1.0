27c49e8f0dcc6c34b14bcf376232f7ba46c00d0e
Jia <michelj9012@gmail.com>
2023-09-17 21:59:51 -0400

    Initial Commit

---
+++ VolunteerProject1.0/VolunteerProject1_0App.swift
@@ -0,0 +1,17 @@
+//
+//  VolunteerProject1_0App.swift
+//  VolunteerProject1.0
+//
+//  Created by Jia Michel on 9/17/23.
+//
+
+import SwiftUI
+
+@main
+struct VolunteerProject1_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
